4a7b20748780820f7e027bdd8b7e94c28c659040
leandro.jm <leandro.jm@gmail.com>
2023-08-08 23:54:36 -0300

    first commit

---
+++ configuration/config.js
@@ -0,0 +1,9 @@
+module.exports={
+    "google_api_key": "247242260822-thblgpbep6g8e6d3fd3niklkpnaq2smb.apps.googleusercontent.com",
+    "google_api_secret": "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",
+    "google_callback_url": "http://localhost:3000/auth/google/callback",
+
+    "facebook_api_key": "1249601685702535",
+    "facebook_api_secret": "0bb79455c6ea7862b79a71abc76706e1",
+    "facebook_callback_url": "http://localhost:3000/auth/facebook/callback"
+}
